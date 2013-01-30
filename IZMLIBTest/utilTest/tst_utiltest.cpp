af8035fe066bd9f6c23d53a99093eb224c4ca3a2
izumi <izumi.mite@gmail.com>
2013-01-30 20:39:45 +0900

    trim(),ltrim(),rtrim()のテストコード追加.

--- IZMLIBTest/utilTest/tst_utiltest.cpp
+++ IZMLIBTest/utilTest/tst_utiltest.cpp
@@ -19,6 +19,7 @@ private Q_SLOTS:
     void cleanup();
 
     void test_split();
+    void test_trim_ltrim_rtrim();
 };
 
 UtilTest::UtilTest()
@@ -62,6 +63,28 @@ void UtilTest::test_split()
     QCOMPARE( strList.at(0).c_str(), "aaa" );
     QCOMPARE( strList.at(1).c_str(), "bbb" );
     QCOMPARE( strList.at(2).c_str(), "ccc" );
+}
+//==============================================================================
+/*
+*/
+void UtilTest::test_trim_ltrim_rtrim()
+{
+    std::string str;
+
+    str = " abc ";
+    QCOMPARE( izm::trim(str),  std::string("abc") );
+    QCOMPARE( izm::ltrim(str), std::string("abc ") );
+    QCOMPARE( izm::rtrim(str), std::string(" abc") );
+
+    str = "   abc   ";
+    QCOMPARE( izm::trim(str),  std::string("abc") );
+    QCOMPARE( izm::ltrim(str), std::string("abc   ") );
+    QCOMPARE( izm::rtrim(str), std::string("   abc") );
+
+    str = " \f\n\r\t\vAAA \f\n\r\t\v";
+    QCOMPARE( izm::trim(str),  std::string("AAA") );
+    QCOMPARE( izm::ltrim(str), std::string("AAA \f\n\r\t\v") );
+    QCOMPARE( izm::rtrim(str), std::string(" \f\n\r\t\vAAA") );
 }
 
 QTEST_APPLESS_MAIN(UtilTest);
