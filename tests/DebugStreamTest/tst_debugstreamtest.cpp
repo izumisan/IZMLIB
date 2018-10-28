3fba502c6cb2632490e61c0bd6b1855ab2c734bd
izumisan <izumi.mite@gmail.com>
2018-10-29 00:58:54 +0900

    [wip] add manipulator test

--- tests/DebugStreamTest/tst_debugstreamtest.cpp
+++ tests/DebugStreamTest/tst_debugstreamtest.cpp
@@ -1,9 +1,11 @@
 #include <QString>
 #include <QtTest>
 #include <QFile>
+#include <QList>
+#include <QTextStream>
 #include <QDebug>
 
-#include "debugstream.h"
+#include "dbg.h"
 
 class DebugStreamTest : public QObject
 {
@@ -20,6 +22,15 @@ private Q_SLOTS:
 
     void should_be_set_default_path();
     void should_be_set_assigned_path();
+
+    void endlTest();
+    void timestampTest();
+    void callerInfoTest();
+    void printfTest();
+    void everyTest();
+
+private:
+    QList<QString> fileContents( const QString& path ) const;
 };
 
 DebugStreamTest::DebugStreamTest()
@@ -46,12 +57,13 @@ void DebugStreamTest::cleanup()
 */
 void DebugStreamTest::should_be_set_default_path()
 {
-    const QString expected( "izm.debug.log" );
-    if ( QFile::exists( expected ) )
+    const auto&& path = std::string( "izm.debug.log" );
+    QFile logfile( path.c_str() );
+    if ( logfile.exists() )
     {
-        QFile::remove( expected );
+        logfile.remove();
     }
-    QCOMPARE( QFile::exists( expected ), false );
+    QCOMPARE( logfile.exists(), false );
 
     auto&& stream = izm::dbg::DebugStream();
     stream << "Hello World";
@@ -61,22 +73,23 @@ void DebugStreamTest::should_be_set_default_path()
     stream << "double:" << 1.1;
     stream.eol();
 
-    QCOMPARE( stream.path().c_str(), expected );
-    QCOMPARE( QFile::exists( expected ), true );
+    QCOMPARE( stream.path().c_str(), path.c_str() );
+    QCOMPARE( logfile.exists(), true );
 }
 //==============================================================================
 /*!
 */
 void DebugStreamTest::should_be_set_assigned_path()
 {
-    const QString&& expected = QString( QTest::currentTestFunction() ) + ".log";
-    if ( QFile::exists( expected ) )
+    const auto&& path = std::string( QTest::currentTestFunction() ) + ".log";
+    QFile logfile( path.c_str() );
+    if ( logfile.exists() )
     {
-        QFile::remove( expected );
+        logfile.remove();
     }
-    QCOMPARE( QFile::exists( expected ), false );
+    QCOMPARE( logfile.exists(), false );
 
-    auto&& stream = izm::dbg::DebugStream( expected.toStdString() );
+    auto&& stream = izm::dbg::DebugStream( path );
     stream << QTest::currentTestFunction();
     stream.eol();
     stream << "int:" << 1;
@@ -84,10 +97,113 @@ void DebugStreamTest::should_be_set_assigned_path()
     stream << "double:" << 1.1;
     stream.eol();
 
-    QCOMPARE( stream.path().c_str(), expected );
-    QCOMPARE( QFile::exists( expected ), true );
+    QCOMPARE( stream.path().c_str(), path.c_str() );
+    QCOMPARE( logfile.exists(), true );
 }
+//==============================================================================
+/*!
+*/
+void DebugStreamTest::endlTest()
+{
+    const auto&& path = std::string( QTest::currentTestFunction() ) + ".log";
+    QFile logfile( path.c_str() );
+    if ( logfile.exists() )
+    {
+        logfile.remove();
+    }
+    QCOMPARE( logfile.exists(), false );
 
+    auto&& stream = izm::dbg::DebugStream( path );
+    for ( int i = 0; i < 10; ++i )
+    {
+        stream << i << izm::endl;
+    }
+
+    QCOMPARE( logfile.exists(), true );
+
+    auto&& contents = fileContents( logfile.fileName() );
+    QCOMPARE( contents.size(), 10 );
+}
+//==============================================================================
+/*!
+*/
+void DebugStreamTest::timestampTest()
+{
+    const auto&& path = std::string( QTest::currentTestFunction() ) + ".log";
+    QFile logfile( path.c_str() );
+    if ( logfile.exists() )
+    {
+        logfile.remove();
+    }
+    QCOMPARE( logfile.exists(), false );
+
+    //@@@ TODO
+}
+//==============================================================================
+/*!
+*/
+void DebugStreamTest::callerInfoTest()
+{
+    const auto&& path = std::string( QTest::currentTestFunction() ) + ".log";
+    QFile logfile( path.c_str() );
+    if ( logfile.exists() )
+    {
+        logfile.remove();
+    }
+    QCOMPARE( logfile.exists(), false );
+
+    //@@@ TODO
+}
+//==============================================================================
+/*!
+*/
+void DebugStreamTest::printfTest()
+{
+    const auto&& path = std::string( QTest::currentTestFunction() ) + ".log";
+    QFile logfile( path.c_str() );
+    if ( logfile.exists() )
+    {
+        logfile.remove();
+    }
+    QCOMPARE( logfile.exists(), false );
+
+    //@@@ TODO
+}
+//==============================================================================
+/*!
+*/
+void DebugStreamTest::everyTest()
+{
+    const auto&& path = std::string( QTest::currentTestFunction() ) + ".log";
+    QFile logfile( path.c_str() );
+    if ( logfile.exists() )
+    {
+        logfile.remove();
+    }
+    QCOMPARE( logfile.exists(), false );
+
+    //@@@ TODO
+}
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+/*!
+*/
+QList<QString> DebugStreamTest::fileContents( const QString& path ) const
+{
+    QList<QString> ret = {};
+
+    QFile file( path );
+    if ( file.open( QIODevice::ReadOnly | QIODevice::Text ) )
+    {
+        QTextStream ts( &file );
+        while ( !ts.atEnd() )
+        {
+            auto&& line = ts.readLine();
+            ret.append( line );
+        }
+    }
+
+    return ret;
+}
 
 QTEST_APPLESS_MAIN(DebugStreamTest)
 
